ddee882976c93eeea208937e289193132e7926f4
Md. Jahid Hasan Naiem 1620266642 <jahid.naiem@northsouth.edu>
2019-11-19 22:06:26 +0600

    Update

--- app/src/androidTest/java/com/example/multithreaded_webcrawler/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/multithreaded_webcrawler/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.multithreaded_webcrawler;
 
 import android.content.Context;
 
--- app/src/main/java/com/example/multithreaded_webcrawler/MainActivity.java
+++ app/src/main/java/com/example/multithreaded_webcrawler/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.multithreaded_webcrawler;
 
 import android.graphics.Color;
 import android.os.AsyncTask;
